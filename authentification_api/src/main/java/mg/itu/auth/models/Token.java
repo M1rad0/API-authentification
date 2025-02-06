faaa1bc3969ddde4dd39926bc1b06295a8abe12e
Mirado <kainrak312@gmail.com>
2025-02-06 22:25:05 +0300

    Création des entités de base

---
+++ authentification_api/src/main/java/mg/itu/auth/models/Token.java
@@ -0,0 +1,89 @@
+package mg.itu.auth.models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Token")
+public class Token {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_token")
+    private Long id;
+
+    @Column(name = "token_value", nullable = false, unique = true, length = 255)
+    private String valeur;
+
+    @Column(name = "code_validation")
+    private Short codeValidation;
+
+    @Column(name = "login_valide", nullable = false)
+    private boolean isValide;
+
+    @Column(name = "date_expiration", nullable = false)
+    private LocalDateTime expiration;
+
+    @ManyToOne
+    @JoinColumn(name = "id_utilisateur", nullable = false)
+    private Utilisateur utilisateur;
+
+    public Token() {}
+
+    public Token(String valeur, Short codeValidation, boolean isValide, LocalDateTime expiration, Utilisateur utilisateur) {
+        this.valeur = valeur;
+        this.codeValidation = codeValidation;
+        this.isValide = isValide;
+        this.expiration = expiration;
+        this.utilisateur = utilisateur;
+    }
+    // Getters et Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(String valeur) {
+        this.valeur = valeur;
+    }
+
+    public Short getCodeValidation() {
+        return codeValidation;
+    }
+
+    public void setCodeValidation(Short codeValidation) {
+        this.codeValidation = codeValidation;
+    }
+
+    public boolean isValide() {
+        return isValide;
+    }
+
+    public void setValide(boolean isValide) {
+        this.isValide = isValide;
+    }
+
+    public LocalDateTime getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(LocalDateTime expiration) {
+        this.expiration = expiration;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }    
+}
---
+++ authentification_api/src/main/java/mg/itu/auth/repositories/TokenRepository.java
@@ -0,0 +1,13 @@
+package mg.itu.auth.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import mg.itu.auth.models.Token;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+    
+}
